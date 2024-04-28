4d1456ff68a4998f9b6968a8d06415e8352dd1bb
Bian Lee <bianlee2004@gmail.com>
2024-04-28 10:49:20 -0700

    transcription

---
+++ app/pages/temp.js
@@ -0,0 +1,44 @@
+import React, { useEffect, useState } from "react";
+import "../src/app/globals.css";
+
+export default function app() {
+  const [message, setMessage] = useState("Loading");
+  const [people, setPeople] = useState([]);
+  const [inputValue, setInputValue] = useState("");
+  const [result, setResult] = useState(null);
+
+  const handleInputChange = (e) => {
+    setInputValue(e.target.value);
+  };
+
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    fetch("http://localhost:8080/api/generate_music", {
+      method: "POST",
+      headers: {
+        "Content-Type": "application/json",
+      },
+      body: JSON.stringify({ text: inputValue }), // Changed to send text
+    })
+      .then((response) => response.json())
+      .then((data) => {
+        setResult(data.result);
+      })
+      .catch((error) => console.error("Error:", error));
+  };
+
+  return (
+    <div>
+      <div>{message}</div>
+      {people.map((person, index) => (
+        <div key={index}>{person}</div>
+      ))}
+      <form onSubmit={handleSubmit}>
+        <input type="text" value={inputValue} onChange={handleInputChange} />{" "}
+        {/* Changed type to text */}
+        <button type="submit">Submit</button>
+      </form>
+      {result !== null && <div>Result: {result}</div>}
+    </div>
+  );
+}
